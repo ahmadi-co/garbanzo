fc03aa855e6dac9b9de2c1187ae0c330d8f10194
Nasirullah Ahmadi <n.ahmadi@minefintech.com>
2025-11-10 13:37:03 +0200

    commit

--- src/components/VerticalWaveBlobs/VerticalWaveBlobs.js
+++ src/components/VerticalWaveBlobs/VerticalWaveBlobs.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './VerticalWaveBlobs.css';
-import MinimalBlobBackground from "../BlobBackground/BlobBackground";
 
 export default function VerticalWaveBlobs() {
     return (
