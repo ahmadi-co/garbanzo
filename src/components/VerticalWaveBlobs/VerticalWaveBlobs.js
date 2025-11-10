bb5b74793ac32d27c65ef8bfbae876d71e549f8a
Nasirullah Ahmadi <n.ahmadi@minefintech.com>
2025-11-10 14:22:46 +0200

    commit

--- src/components/VerticalWaveBlobs/VerticalWaveBlobs.js
+++ src/components/VerticalWaveBlobs/VerticalWaveBlobs.js
@@ -1,31 +1,12 @@
-import React, { useRef, useEffect } from "react";
+import React from 'react';
 import './VerticalWaveBlobs.css';
+import MinimalBlobBackground from "../BlobBackground/BlobBackground";
 
 export default function VerticalWaveBlobs() {
-    const svgRef = useRef();
-
-    useEffect(() => {
-        const svg = svgRef.current;
-
-        if (!svg) return;
-
-        // Pause animations when offscreen
-        const observer = new IntersectionObserver(([entry]) => {
-            if (entry.isIntersecting) {
-                svg.unpauseAnimations();
-            } else {
-                svg.pauseAnimations();
-            }
-        });
-
-        observer.observe(svg);
-
-        return () => observer.disconnect();
-    }, []);
-
     return (
         <div className="vwb-container">
-            <svg ref={svgRef} className="vwb-svg" xmlns="http://www.w3.org/2000/svg">
+            <MinimalBlobBackground/>
+            <svg className="vwb-svg" xmlns="http://www.w3.org/2000/svg">
                 <defs>
                     {/* Line Gradients */}
                     <linearGradient id="vLineGrad1" x1="0%" y1="100%" x2="0%" y2="0%">
@@ -52,10 +33,12 @@ export default function VerticalWaveBlobs() {
                     {/* Accent Gradient */}
                     <linearGradient id="accentGrad" x1="0%" y1="0%" x2="100%" y2="100%">
                         <stop offset="0%" stopColor="#4a6bbd" stopOpacity="0.6">
-                            <animate attributeName="stop-color" values="#4a6bbd;#7094d4;#4a6bbd" dur="6s" repeatCount="indefinite"/>
+                            <animate attributeName="stop-color" values="#4a6bbd;#7094d4;#4a6bbd" dur="6s"
+                                     repeatCount="indefinite"/>
                         </stop>
                         <stop offset="100%" stopColor="#7094d4" stopOpacity="0.6">
-                            <animate attributeName="stop-color" values="#7094d4;#4a6bbd;#7094d4" dur="6s" repeatCount="indefinite"/>
+                            <animate attributeName="stop-color" values="#7094d4;#4a6bbd;#7094d4" dur="6s"
+                                     repeatCount="indefinite"/>
                         </stop>
                     </linearGradient>
 
@@ -73,21 +56,12 @@ export default function VerticalWaveBlobs() {
                         <stop offset="100%" stopColor="#5c7fc7" stopOpacity="0"/>
                     </radialGradient>
 
-                    {/* Filters - Reduced Gaussian blur to improve performance */}
-                    <filter id="vLineGlow">
-                        <feGaussianBlur stdDeviation="1.5" result="coloredBlur"/>
-                        <feMerge>
-                            <feMergeNode in="coloredBlur"/>
-                            <feMergeNode in="SourceGraphic"/>
-                        </feMerge>
-                    </filter>
-
                     <filter id="vBlobBlur">
-                        <feGaussianBlur stdDeviation="3"/>
+                        <feGaussianBlur stdDeviation="7"/>
                     </filter>
 
                     <filter id="strongGlow">
-                        <feGaussianBlur stdDeviation="3" result="coloredBlur"/>
+                        <feGaussianBlur stdDeviation="5" result="coloredBlur"/>
                         <feMerge>
                             <feMergeNode in="coloredBlur"/>
                             <feMergeNode in="coloredBlur"/>
@@ -98,6 +72,7 @@ export default function VerticalWaveBlobs() {
 
                 {/* Animated Lines */}
                 <g filter="url(#vLineGlow)">
+                    {/* Vertical Line 1 */}
                     <path
                         d="M200,1500 Q180,1300 200,1100 T200,700 Q220,500 200,300 T200,-100"
                         fill="none"
@@ -110,13 +85,14 @@ export default function VerticalWaveBlobs() {
                             dur="8s"
                             repeatCount="indefinite"
                             values="
-M200,1500 Q180,1300 200,1100 T200,700 Q220,500 200,300 T200,-100;
-M220,1500 Q200,1300 180,1100 T220,700 Q200,500 220,300 T200,-100;
-M200,1500 Q180,1300 200,1100 T200,700 Q220,500 200,300 T200,-100
-"
+      M200,1500 Q180,1300 200,1100 T200,700 Q220,500 200,300 T200,-100;
+      M220,1500 Q200,1300 180,1100 T220,700 Q200,500 220,300 T200,-100;
+      M200,1500 Q180,1300 200,1100 T200,700 Q220,500 200,300 T200,-100
+    "
                         />
                     </path>
 
+                    {/* Vertical Line 2 */}
                     <path
                         d="M400,1600 Q420,1400 400,1200 T400,800 Q380,600 400,400 T400,-100"
                         fill="none"
@@ -129,13 +105,14 @@ M200,1500 Q180,1300 200,1100 T200,700 Q220,500 200,300 T200,-100
                             dur="10s"
                             repeatCount="indefinite"
                             values="
-M400,1600 Q420,1400 400,1200 T400,800 Q380,600 400,400 T400,-100;
-M380,1600 Q400,1400 420,1200 T380,800 Q400,600 380,400 T400,-100;
-M400,1600 Q420,1400 400,1200 T400,800 Q380,600 400,400 T400,-100
-"
+      M400,1600 Q420,1400 400,1200 T400,800 Q380,600 400,400 T400,-100;
+      M380,1600 Q400,1400 420,1200 T380,800 Q400,600 380,400 T400,-100;
+      M400,1600 Q420,1400 400,1200 T400,800 Q380,600 400,400 T400,-100
+    "
                         />
                     </path>
 
+                    {/* Vertical Line 3 */}
                     <path
                         d="M700,1550 Q680,1350 700,1150 T700,750 Q720,550 700,350 T700,-100"
                         fill="none"
@@ -148,13 +125,14 @@ M400,1600 Q420,1400 400,1200 T400,800 Q380,600 400,400 T400,-100
                             dur="12s"
                             repeatCount="indefinite"
                             values="
-M700,1550 Q680,1350 700,1150 T700,750 Q720,550 700,350 T700,-100;
-M720,1550 Q700,1350 680,1150 T720,750 Q700,550 720,350 T700,-100;
-M700,1550 Q680,1350 700,1150 T700,750 Q720,550 700,350 T700,-100
-"
+      M700,1550 Q680,1350 700,1150 T700,750 Q720,550 700,350 T700,-100;
+      M720,1550 Q700,1350 680,1150 T720,750 Q700,550 720,350 T700,-100;
+      M700,1550 Q680,1350 700,1150 T700,750 Q720,550 700,350 T700,-100
+    "
                         />
                     </path>
 
+                    {/* Vertical Line 4 */}
                     <path
                         d="M1000,1650 Q1020,1450 1000,1250 T1000,850 Q980,650 1000,450 T1000,-100"
                         fill="none"
@@ -167,10 +145,10 @@ M700,1550 Q680,1350 700,1150 T700,750 Q720,550 700,350 T700,-100
                             dur="15s"
                             repeatCount="indefinite"
                             values="
-M1000,1650 Q1020,1450 1000,1250 T1000,850 Q980,650 1000,450 T1000,-100;
-M980,1650 Q1000,1450 1020,1250 T980,850 Q1000,650 980,450 T1000,-100;
-M1000,1650 Q1020,1450 1000,1250 T1000,850 Q980,650 1000,450 T1000,-100
-"
+      M1000,1650 Q1020,1450 1000,1250 T1000,850 Q980,650 1000,450 T1000,-100;
+      M980,1650 Q1000,1450 1020,1250 T980,850 Q1000,650 980,450 T1000,-100;
+      M1000,1650 Q1020,1450 1000,1250 T1000,850 Q980,650 1000,450 T1000,-100
+    "
                         />
                     </path>
                 </g>
